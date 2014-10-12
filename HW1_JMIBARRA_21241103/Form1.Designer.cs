dec303b0afe2c0b4690ff351927ad1bc3b50f12d
jmibarrad <jmibarra@unitec.edu>
2014-10-11 21:13:14 -0600

    Only Arabic2Roman
    Solo se puede de numeros arabigos a romanos

---
+++ HW1_JMIBARRA_21241103/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace HW1_JMIBARRA_21241103
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.OldLace;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(40, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 18);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(105, 57);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Location = new System.Drawing.Point(6, 45);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(74, 27);
+            this.cmbFrom.TabIndex = 4;
+            // 
+            // txtAns
+            // 
+            this.txtAns.Enabled = false;
+            this.txtAns.Location = new System.Drawing.Point(120, 156);
+            this.txtAns.Name = "txtAns";
+            this.txtAns.Size = new System.Drawing.Size(333, 20);
+            this.txtAns.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(26, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Answer:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbTo);
+            this.groupBox1.Controls.Add(this.cmbFrom);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(253, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 83);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Convert";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(116, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "To:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "From:";
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Location = new System.Drawing.Point(120, 45);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(74, 27);
+            this.cmbTo.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(69, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(290, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Arabic         Roman";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(105, 83);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(100, 18);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::HW1_JMIBARRA_21241103.Properties.Resources.reload_md;
+            this.pictureBox2.Location = new System.Drawing.Point(208, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(465, 205);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAns);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "A2R";
+            this.TransparencyKey = System.Drawing.Color.Turquoise;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ HW1_JMIBARRA_21241103/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using kXtensions;
+
+namespace HW1_JMIBARRA_21241103
+{
+    public enum A2R_Values
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000
+    }
+
+    public partial class Form1 : Form
+    {
+        private const int EM_SETCUEBANNER = 0x1501;
+        private static ArrayList AL = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Init Values cmb
+            cmbFrom.Items.Add("Arabic");
+            cmbFrom.Items.Add("Roman");
+            cmbTo.Items.Add("Arabic");
+            cmbTo.Items.Add("Roman");
+
+            SendMessage(txtInput.Handle, EM_SETCUEBANNER, 0, "Input here...");
+            txtAns.Text = "Wait for Answer...";
+            cmbFrom.Text = "Arabic";
+            cmbTo.Text = "Roman";
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam,
+            [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        public static Boolean Validate_Arabic(string from)
+        {
+            if (!from.IsNumber())
+            {
+                MessageBox.Show("ERROR: ONLY NUMBERS", "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool Validate_Range(string number)
+        {
+            int x= Int32.Parse(number);
+            if (!Enumerable.Range(1, 3999).Contains(x))
+            {
+                MessageBox.Show("ERROR: Range(1 to 3999)","Range Error",MessageBoxButtons.OK,MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button1);
+                return false;
+            }
+            return true;
+        }
+
+
+        public static int Positional_Value(int pos)
+        {
+            int value=-1;  
+            switch (pos)
+            {
+                case 4:
+                    value = 1000;
+                    break;
+                case 3:
+                    value = 100;
+                    break;
+                case 2:
+                    value = 10;
+                    break;
+                case 1:
+                    value = 1;
+                    break;
+            }
+            return value;
+        }
+
+        public static string Convert_A2R(string input)
+        {
+            var x = input.Length;
+            ArrayList answer = new ArrayList();
+            answer.Clear();
+            for (var i = 0; i < input.Length; i++)
+                AL.Add(input.ElementAt(i));
+            
+            foreach (var value in AL)
+            {
+               answer.Add(Convert_A2R(Int32.Parse(value.ToString()),Positional_Value(x)));
+                x--;
+            }
+
+            return String.Join(String.Empty, answer.ToArray()); ;
+        }
+
+        public static string Convert_A2R(int value, int posValue)
+        {
+            string nomeclature="";
+            switch (posValue)
+            {
+                case 1:
+                    switch (value)
+                    {
+                        case 1: nomeclature = A2R_Values.I.ToString();
+                            break;
+                        case 2: nomeclature = A2R_Values.I + A2R_Values.I.ToString();
+                            break;
+                        case 3: nomeclature = A2R_Values.I.ToString()+A2R_Values.I+A2R_Values.I;
+                            break;
+                        case 4: nomeclature = A2R_Values.I+A2R_Values.V.ToString();
+                            break;
+                        case 5: nomeclature = A2R_Values.V.ToString();
+                            break;
+                        case 6: nomeclature = A2R_Values.V + A2R_Values.I.ToString();
+                            break;
+                        case 7: nomeclature = A2R_Values.V + A2R_Values.I.ToString()+ A2R_Values.I;
+                            break;
+                        case 8: nomeclature = A2R_Values.V + A2R_Values.I.ToString() + A2R_Values.I+ A2R_Values.I;
+                            break;
+                        case 9: nomeclature = A2R_Values.I + A2R_Values.X.ToString();
+                            break;
+                    }
+                    break;
+                case 10:
+                    switch (value)
+                    {
+                        case 1: nomeclature = A2R_Values.X.ToString();
+                            break;
+                        case 2: nomeclature = A2R_Values.X + A2R_Values.X.ToString();
+                            break;
+                        case 3: nomeclature = A2R_Values.X.ToString() + A2R_Values.X + A2R_Values.X;
+                            break;
+                        case 4: nomeclature = A2R_Values.X.ToString() + A2R_Values.L;
+                            break;
+                        case 5: nomeclature = A2R_Values.L.ToString();
+                            break;
+                        case 6: nomeclature = A2R_Values.L + A2R_Values.X.ToString();
+                            break;
+                        case 7: nomeclature = A2R_Values.L + A2R_Values.X.ToString() + A2R_Values.X;
+                            break;
+                        case 8: nomeclature = A2R_Values.L + A2R_Values.X.ToString() + A2R_Values.X + A2R_Values.X;
+                            break;
+                        case 9: nomeclature = A2R_Values.X + A2R_Values.C.ToString();
+                            break;
+                    }
+                    break;
+                case 100:
+                    switch (value)
+                    {
+                        case 1: nomeclature = A2R_Values.C.ToString();
+                            break;
+                        case 2: nomeclature = A2R_Values.C + A2R_Values.C.ToString();
+                            break;
+                        case 3: nomeclature = A2R_Values.C.ToString() + A2R_Values.C + A2R_Values.C;
+                            break;
+                        case 4: nomeclature = A2R_Values.C + A2R_Values.D.ToString();
+                            break;
+                        case 5: nomeclature = A2R_Values.D.ToString();
+                            break;
+                        case 6: nomeclature = A2R_Values.D + A2R_Values.C.ToString();
+                            break;
+                        case 7: nomeclature = A2R_Values.D + A2R_Values.C.ToString() + A2R_Values.C;
+                            break;
+                        case 8: nomeclature = A2R_Values.D + A2R_Values.C.ToString() + A2R_Values.C + A2R_Values.C;
+                            break;
+                        case 9: nomeclature = A2R_Values.C + A2R_Values.M.ToString();
+                            break;
+                    }
+                    break;
+                case 1000:
+                    switch (value)
+                    {
+                        case 1: nomeclature = A2R_Values.M.ToString();
+                            break;
+                        case 2: nomeclature = A2R_Values.M + A2R_Values.M.ToString();
+                            break;
+                        case 3: nomeclature = A2R_Values.M + A2R_Values.M.ToString()+A2R_Values.M;
+                            break;
+                    }
+                    break;
+            }
+            return nomeclature;
+        }
+
+        public static string Convert_R2A(string Input)
+        {
+            return "";
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            txtAns.Clear();
+            Boolean IsNum;
+            Boolean IsRanged=false;
+            if (!cmbFrom.Text.Equals("") && !cmbTo.Text.Equals(""))
+                if (cmbFrom.Text.Equals("Arabic"))
+                {
+                     IsNum=Validate_Arabic(txtInput.Text);
+                    if (IsNum == true)
+                        IsRanged=Validate_Range(txtInput.Text);
+
+                }
+
+            if (IsRanged)
+               txtAns.Text = Convert_A2R(txtInput.Text);
+            
+        }
+
+    }
+}
